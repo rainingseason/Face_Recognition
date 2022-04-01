5f9d7a9a833eba9e092b57bc9f7748f4db29bc3a
rainingseason <akhoo004@e.ntu.edu.sg>
2022-04-01 11:17:48 +0800

    For Alyna to run

--- KailunavatarCode/edge_detector.py
+++ KailunavatarCode/edge_detector.py
@@ -1,29 +1,41 @@
-import numpy as np
 import cv2
 from scipy.ndimage import convolve
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import KailunavatarCode.cannyfunction as cf
 import numpy as np
+import os
 
+# path = os.getcwd()
+# os.chdir(path + "\\KailunavatarCode")
 
 img = cv2.imread('lady1.JPG')
-# print(img)
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_img = cv2.imread('lady1.JPG', 0) # flag = 0 reads in grayscale
+# img = cv2.imread('lady1.JPG')
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# pre-defined function
+# edges = cv2.Canny(img,100,200)
+# plt.subplot(121),plt.imshow(img,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
 dim1 = (3000, 3000)
 resized = cv2.resize(gray_img, dim1)
 
-# edges = cv2.Canny(resized,100,200)
 result = convolve(gray_img, cf.gaussian_kernel())
 (result, theta) = cf.sobel_filters(result)
-# result = cf.non_max_suppression(result, theta)
+result = cf.non_max_suppression(result, theta)
+# round_result = np.rint(result)
 
 # plt.subplot(121)
 # plt.imshow(img)
 # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(gray_img)
+# plt.subplot(122),plt.imshow(result)
 # plt.title('Result Image'), plt.xticks([]), plt.yticks([])
 # plt.show()
 
 cv2.imshow("Original", img)
-cv2.imshow("Gray", gray_img)
+cv2.imshow("Result", round_result)
 cv2.waitKey(0)
