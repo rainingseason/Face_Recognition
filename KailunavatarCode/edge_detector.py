5c6d31eef340cfccbdbdea24f677b47ff8ae217c
rainingseason <akhoo004@e.ntu.edu.sg>
2022-04-04 14:44:06 +0800

    For Alyna to run

--- KailunavatarCode/edge_detector.py
+++ KailunavatarCode/edge_detector.py
@@ -28,13 +28,13 @@ result = convolve(gray_img, cf.gaussian_kernel())
 (result, theta) = cf.sobel_filters(result)
 result = cf.non_max_suppression(result, theta)
 
-threshold = 0.95
-
-thresholded = result / result.max()
-low_values_flags = thresholded < threshold  # values are below threshold
-thresholded[low_values_flags] = 0  # All low values set to 0
-high_values_flags = thresholded > threshold
-thresholded[high_values_flags] = 1
+# threshold = 0.98
+#
+# thresholded = result / result.max()
+# low_values_flags = thresholded < threshold  # values are below threshold
+# thresholded[low_values_flags] = 0  # All low values set to 0
+# high_values_flags = thresholded > threshold
+# thresholded[high_values_flags] = 1
 
 # round_result = np.rint(result)
 
@@ -51,6 +51,30 @@ thresholded[high_values_flags] = 1
 # plt.show()
 
 cv2.imshow("Original", img)
-cv2.imshow("Result", thresholded)
+cv2.imshow("Result", np.rint(result))
+# cv2.imshow("Thresholded", thresholded)
 # cv2.imshow("Author", nonMaxImg)
 cv2.waitKey(0)
+
+def sobel(img):
+    Kx = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], np.float32)
+    Ky = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+
+    conv_img_x = 0
+    conv_img_y = 0
+
+    try:
+        for row in range(len(img)):
+            for column in range(len(img[row])):
+                output_x = 0
+                output_y = 0
+                for pixel_row in range(3):
+                    for pixel_column in range(3):
+                        output_x += Kx[pixel_row][pixel_column] * img[row - pixel_row - 1][column - pixel_column - 1]
+                        output_y += Ky[pixel_row][pixel_column] * img[row - pixel_row - 1][column - pixel_column - 1]
+                conv_img_x[row][column] = output_x
+                conv_img_y[row][column] = output_y
+
+    except IndexError:
+        pass
+    return conv_img_x, conv_img_y
