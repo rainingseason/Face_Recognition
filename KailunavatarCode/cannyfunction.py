7a209b618384552c681b7b62fbfc39093c7df7ae
rainingseason <akhoo004@e.ntu.edu.sg>
2022-04-07 21:49:04 +0800

    For Alyna to run

--- KailunavatarCode/cannyfunction.py
+++ KailunavatarCode/cannyfunction.py
@@ -1,27 +1,23 @@
-from scipy import ndimage
-from scipy.ndimage.filters import convolve
-
-from scipy import misc
 import numpy as np
+from scipy import signal
 
-# self, imgs, sigma=1, kernel_size=5, weak_pixel=75, strong_pixel=255, lowthreshold=0.05, highthreshold=0.15
-
-def gaussian_kernel(size=5, sigma=1):
-    size = int(size) // 2
-    x, y = np.mgrid[-size:size + 1, -size:size + 1]
-    normal = 1 / (2.0 * np.pi * sigma ** 2)
-    g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2))) * normal
+# gaussian smoothing
+def gaussian_kernel(size = 5, sigma = 1.3):
+    size = int(size) // 2 # floor division eg. 5 // 2 = 2.5 -> 2
+    x, y = np.mgrid[-size:size+1, -size:size+1]
+    normal = 1 / (2.0 * np.pi * sigma**2)
+    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2)))*normal
     return g
 
 def sobel_filters(img):
-    Kx = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], np.float32)
-    Ky = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], np.float32)
 
-    Ix = ndimage.filters.convolve(img, Kx)
-    Iy = ndimage.filters.convolve(img, Ky)
+    Ix = signal.convolve2d(img, Kx, boundary='fill', mode='same')
+    Iy = signal.convolve2d(img, Ky, boundary='fill', mode='same')
 
     G = np.hypot(Ix, Iy)
-    G = G / G.max() * 255
+    G = G / G.max() * 255.0
     theta = np.arctan2(Iy, Ix)
     return (G, theta)
 
@@ -59,8 +55,43 @@ def non_max_suppression(img, D):
                 else:
                     Z[i, j] = 0
 
-
             except IndexError as e:
                 pass
-
     return Z
+
+
+def threshold(img, lowThresholdRatio=0.05, highThresholdRatio=0.1):
+    highThreshold = img.max() * highThresholdRatio;
+    lowThreshold = highThreshold * lowThresholdRatio;
+
+    M, N = img.shape
+    res = np.zeros((M, N), dtype=np.int32)
+
+    weak = np.int32(25)
+    strong = np.int32(255)
+
+    strong_i, strong_j = np.where(img >= highThreshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+
+    return (res, weak, strong)
+
+def hysteresis(img, weak, strong=255):
+    M, N = img.shape
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            if (img[i,j] == weak):
+                try:
+                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
+                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
+                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
+                        img[i, j] = strong
+                    else:
+                        img[i, j] = 0
+                except IndexError as e:
+                    pass
+    return img
--- KailunavatarCode/edge_detector.py
+++ KailunavatarCode/edge_detector.py
@@ -1,80 +1,32 @@
 import cv2
-from scipy.ndimage import convolve
-import matplotlib.pyplot as plt
+from scipy import signal
+from matplotlib import pyplot as plt
 import KailunavatarCode.cannyfunction as cf
-import numpy as np
 import os
 
-# path = os.getcwd()
-# os.chdir(path + "\\KailunavatarCode")
+def toFitMatplotlib(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
-img = cv2.imread('lady1.JPG')
-gray_img = cv2.imread('lady1.JPG', 0) # flag = 0 reads in grayscale
-# img = cv2.imread('lady1.JPG')
-# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# pre-processing
+path = os.getcwd()
+path = path + '\\KailunavatarCode\\lady1.JPG'
+img = cv2.imread(path) # BGR
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# pre-defined function
-# edges = cv2.Canny(img,100,200)
-# plt.subplot(121),plt.imshow(img,cmap = 'gray')
-# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-# plt.show()
+gaussian_smooth = signal.convolve2d(gray_img, cf.gaussian_kernel(), boundary='fill', mode='same')
 
-dim1 = (3000, 3000)
-resized = cv2.resize(gray_img, dim1)
+(result, theta) = cf.sobel_filters(gray_img)
 
-result = convolve(gray_img, cf.gaussian_kernel())
-(result, theta) = cf.sobel_filters(result)
 result = cf.non_max_suppression(result, theta)
+result, weak, strong = cf.threshold(result)
 
-# threshold = 0.98
-#
-# thresholded = result / result.max()
-# low_values_flags = thresholded < threshold  # values are below threshold
-# thresholded[low_values_flags] = 0  # All low values set to 0
-# high_values_flags = thresholded > threshold
-# thresholded[high_values_flags] = 1
+plt.subplot(121)
+plt.imshow(gray_img, cmap='gray')
+plt.title('Original Image')
+plt.xticks([]), plt.yticks([])
 
-# round_result = np.rint(result)
-
-# author's code
-# img_smoothed = cf.convolve(resized, cf.gaussian_kernel())
-# gradientMat, thetaMat = cf.sobel_filters(img_smoothed)
-# nonMaxImg = cf.non_max_suppression(gradientMat, thetaMat)
-
-# plt.subplot(121)
-# plt.imshow(img)
-# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(result)
-# plt.title('Result Image'), plt.xticks([]), plt.yticks([])
-# plt.show()
-
-cv2.imshow("Original", img)
-cv2.imshow("Result", np.rint(result))
-# cv2.imshow("Thresholded", thresholded)
-# cv2.imshow("Author", nonMaxImg)
-cv2.waitKey(0)
-
-def sobel(img):
-    Kx = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], np.float32)
-    Ky = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-
-    conv_img_x = 0
-    conv_img_y = 0
-
-    try:
-        for row in range(len(img)):
-            for column in range(len(img[row])):
-                output_x = 0
-                output_y = 0
-                for pixel_row in range(3):
-                    for pixel_column in range(3):
-                        output_x += Kx[pixel_row][pixel_column] * img[row - pixel_row - 1][column - pixel_column - 1]
-                        output_y += Ky[pixel_row][pixel_column] * img[row - pixel_row - 1][column - pixel_column - 1]
-                conv_img_x[row][column] = output_x
-                conv_img_y[row][column] = output_y
-
-    except IndexError:
-        pass
-    return conv_img_x, conv_img_y
+plt.subplot(122),plt.imshow(result, cmap='gray')
+plt.title('Result Image')
+plt.xticks([]), plt.yticks([])
+plt.show()
